27e186e75e331fa0cee5824426d6014d906caba9
parthmagnani <magnani.parth2312@gmail.com>
2024-02-12 23:10:29 +0530

    latest changes

--- src/app/Layout/Components/promises/promises.component.ts
+++ src/app/Layout/Components/promises/promises.component.ts
@@ -7,4 +7,24 @@ import { Component } from '@angular/core';
 })
 export class PromisesComponent {
 
+  generatedValueByPromise: any = null
+
+  promiseExample(){
+    const randomNumber = Math.floor(Math.random() * 10) + 1;
+    return new Promise((resolve, reject) => {
+      if(randomNumber <= 5){
+        setTimeout(() => {
+          this.generatedValueByPromise = randomNumber
+          resolve(this.generatedValueByPromise )
+        }, randomNumber*1000);
+      }else{
+        setTimeout(() => {
+          this.generatedValueByPromise = randomNumber
+          reject(this.generatedValueByPromise)
+        }, 1000);
+      }
+    })
+    
+
+  }
 }
